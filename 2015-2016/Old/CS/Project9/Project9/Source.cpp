b2f6a70e131fe8efe111c701053d87a9288e79e5
Zephyrum-Alsend <isfaryl@gmail.com>
2017-02-25 17:56:01 -0500

    Revert "Revert "Revert "2015-2016 Projects"""
    This reverts commit 8ba8432abe293e72d9ef5326ea84fff830aed520.

--- 2015-2016/Old/CS/Project9/Project9/Source.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-void findavglen(char s[80], float&answer);
-int main(){
-
-}
-void findavglen(char s[80], float&answer){
-
-}
--- 2015-2016/Old/Python Programs/EX12.py
+++
@@ -1,9 +0,0 @@
-def isIn(str1, str2):
-	if str1 in str2:
-		return True
-	elif str2 in str1:
-		return True
-	else:
-		return False
-
-print(isIn("doggod", "dog"))
